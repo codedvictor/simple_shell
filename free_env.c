a7fda43ca0efbdfff1bcae80df45612a1547b06e
Edidiong Offong <donoffonglet_icy@yahoo.co.uk>
2023-04-24 16:42:52 -0700

    free enviroment variable space

---
+++ free_env.c
@@ -0,0 +1,17 @@
+#include "cshell.h"
+
+/**
+  * frees_get_env - entry point
+  * @env_path: The environment variable to frees
+  *
+  * Return: Nothing to returns
+  */
+void frees_get_env(char *env_path)
+{
+        int x;
+
+        for (x = 4; x >= 0; x--)
+                env_path--;
+
+        free(env_path);
+}
