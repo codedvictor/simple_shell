1139f493ba28c0f8ea08397f45df57c603ff0707
Ige Victor <victorkoladeige@gmail.com>
2023-04-26 07:34:07 -0700

    exit debugging

--- _exit.c
+++ _exit.c
@@ -7,32 +7,38 @@
   *
   * Return: Nothing to returns
   */
+
 void _handle_exit(char **u_tok, char *line)
 {
-	int stat = 0;
+        int stat = 0;
 
-	if (u_tok[1] == NULL || (!_strcmp(u_tok[1], "0")))
-	{
-		frees_tokens(u_tok);
-		free(line);
-		exit(0);
-	}
-	stat = _atoi(u_tok[1]);
-	if (stat != 0)
-	{
-		frees_tokens(u_tok);
-		free(line);
-		exit(stat);
-	}
-	else
-	{
-		_puts("exit: Illegal number: ");
-		_puts(u_tok[1]);
-		_puts("\n");
-		exit(2);
-	}
-
-	frees_tokens(u_tok);
-	free(line);
-	exit(EXIT_SUCCESS);
+        if (u_tok[1] == NULL)
+        {
+                free(line);
+                frees_tokens(u_tok);
+                exit(0);
+        }
+        if (!_strcmp(u_tok[1], "0"))
+        {
+                free(line);
+                frees_tokens(u_tok);
+                exit(0);
+        }
+        stat = _atoi(u_tok[1]);
+        if (stat != 0)
+        {
+                free(line);
+                frees_tokens(u_tok);
+                exit(stat);
+        }
+        else
+        {
+                _puts("exit: Illegal number: ");
+                _puts(u_tok[1]);
+                _puts("\n");
+                free(line);
+                frees_tokens(u_tok);
+                exit(2);
+        }
 }
+
